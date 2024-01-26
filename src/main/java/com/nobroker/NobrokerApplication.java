20be39a3617456fa8f97dcdf655b9cc52f70d47c
rekha <rekhapatel1298@gmail.com>
2024-01-26 16:03:44 +0530

    add

---
+++ src/main/java/com/nobroker/NobrokerApplication.java
@@ -0,0 +1,13 @@
+package com.nobroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobrokerApplication.class, args);
+	}
+
+}
