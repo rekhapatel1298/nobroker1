4af78b7a594ad347f1d3c6b3200126a29020a6cf
rekha <rekhapatel1298@gmail.com>
2024-01-27 16:48:44 +0530

    27th Jan 2024

---
+++ src/main/java/com/nobroker/payload/OwnerPlanDto.java
@@ -0,0 +1,33 @@
+package com.nobroker.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OwnerPlanDto {
+
+    private long planId;
+
+    private String planName;
+
+    private double price;
+
+    private  int planValidity;
+
+    private boolean relationshipManager;
+
+    private boolean rentalAgreement;
+
+    private boolean propertyPromotion;
+
+    private  boolean gauranteedTenants;
+
+    private boolean showingProperty;
+
+    private boolean facebookMarketingOfProperty;
+}
+
