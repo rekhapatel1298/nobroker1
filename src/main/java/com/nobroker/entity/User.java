71acc2bbb5b422c8ffc5f619ae08feb5b8a86fa5
rekha <rekhapatel1298@gmail.com>
2024-02-04 16:50:34 +0530

    29/1

--- src/main/java/com/nobroker/entity/User.java
+++ src/main/java/com/nobroker/entity/User.java
@@ -1,9 +1,6 @@
 package com.nobroker.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +12,12 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+   private long id;
     private String name;
+    @Column(unique = true )
     private String email;
     private String mobile;
     private String password;
+    private boolean emailVerificati;
+
 }
